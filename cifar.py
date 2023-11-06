cf94663bcf9c15a72458e49f528d687637863aec
yufu <yufu@github.com>
2023-11-06 11:07:56 +0000

    vit feature extraction

--- cifar.py
+++ cifar.py
@@ -20,22 +20,19 @@ def vit_model_init(device, vit_name='google/vit-large-patch16-384-in21k'):
 
 def main(config, posthoc_bias_correction=False):
     logger = config.get_logger('test')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # setup data_loader instances
-    if 'returns_feat' in config['arch']['args']:
-        model = config.init_obj('arch', module_arch, allow_override=True, returns_feat=False)
-    else:
-        model = config.init_obj('arch', module_arch)
+    model = vit_model_init(device)
 
     logger.info('Loading checkpoint: {} ...'.format(config.resume))
-    checkpoint = torch.load(config.resume)
-    state_dict = checkpoint['state_dict']
+    # checkpoint = torch.load(config.resume)
+    # state_dict = checkpoint['state_dict']
     if config['n_gpu'] > 1:
         model = torch.nn.DataParallel(model)
-    model.load_state_dict(state_dict)
+    # model.load_state_dict(state_dict)
 
     # prepare model for testing
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
     model.eval()
 
@@ -55,98 +52,14 @@ def main(config, posthoc_bias_correction=False):
 
     num_classes = config._config["arch"]["args"]["num_classes"]
 
-    distrb = {
-        'uniform': (0, False),
-        'forward50': (0.02, False),
-        'forward25': (0.04, False),
-        'forward10': (0.1, False),
-        'forward5': (0.2, False),
-        'forward2': (0.5, False),
-        'backward50': (0.02, True),
-        'backward25': (0.04, True),
-        'backward10': (0.1, True),
-        'backward5': (0.2, True),
-        'backward2': (0.5, True),
-    }
-
-    record_list = []
-
-    # test_distribution_set = ["forward50", "forward25", "forward10", "forward5", "forward2", "uniform", "backward2",
-    #                          "backward5", "backward10", "backward25", "backward50"]
-    test_distribution_set = ['uniform']
-    for test_distribution in test_distribution_set:
-        print(test_distribution)
-        data_loader = TestAgnosticImbalanceCIFAR100DataLoader(
-            config['data_loader']['args']['data_dir'],
-            batch_size=128,
-            shuffle=False,
-            training=False,
-            num_workers=2,
-            test_imb_factor=distrb[test_distribution][0],
-            reverse=distrb[test_distribution][1]
-        )
-
-        if posthoc_bias_correction:
-            test_prior = torch.tensor(data_loader.cls_num_list).float().to(device)
-            test_prior = test_prior / test_prior.sum()
-            test_bias = test_prior.log()
-        else:
-            test_bias = None
-
-        adjusted_model = adjusted_model_wrapper(model, test_bias=test_bias)
-
-        record = validation(data_loader, adjusted_model, num_classes, device, many_shot, medium_shot, few_shot)
-
-        record_list.append(record)
-    print('=' * 25, ' Final results ', '=' * 25)
-    i = 0
-    for txt in record_list:
-        print(test_distribution_set[i] + '\t')
-        print(*txt)
-        i += 1
-
-
-def mic_acc_cal(preds, labels):
-    if isinstance(labels, tuple):
-        assert len(labels) == 3
-        targets_a, targets_b, lam = labels
-        acc_mic_top1 = (lam * preds.eq(targets_a.data).cpu().sum().float() \
-                        + (1 - lam) * preds.eq(targets_b.data).cpu().sum().float()) / len(preds)
-    else:
-        acc_mic_top1 = (preds == labels).sum().item() / len(labels)
-    return acc_mic_top1
-
-
-def validation(data_loader, model, num_classes, device, many_shot, medium_shot, few_shot):
-    confusion_matrix = torch.zeros(num_classes, num_classes).cuda()
-    total_logits = torch.empty((0, num_classes)).cuda()
-    total_labels = torch.empty(0, dtype=torch.long).cuda()
+    total_feat = torch.empty((0)).cuda()
     with torch.no_grad():
-        for i, (data, target) in enumerate(tqdm(data_loader)):
+        for i, (data, target) in enumerate(tqdm(train_data_loader)):
             data, target = data.to(device), target.to(device)
-            output = model(data)
-            for t, p in zip(target.view(-1), output.argmax(dim=1).view(-1)):
-                confusion_matrix[t.long(), p.long()] += 1
-            total_logits = torch.cat((total_logits, output))
-            total_labels = torch.cat((total_labels, target))
+            feat = model(data)
+            feat = model(data)
+            total_feat = torch.cat((total_feat, feat))
 
-    probs, preds = F.softmax(total_logits.detach(), dim=1).max(dim=1)
-
-    # Calculate the overall accuracy and F measurement
-    eval_acc_mic_top1 = mic_acc_cal(preds[total_labels != -1],
-                                    total_labels[total_labels != -1])
-
-    acc_per_class = confusion_matrix.diag() / confusion_matrix.sum(1)
-    acc = acc_per_class.cpu().numpy()
-    many_shot_acc = acc[many_shot].mean()
-    medium_shot_acc = acc[medium_shot].mean()
-    few_shot_acc = acc[few_shot].mean()
-    print("Many-shot {}, Medium-shot {}, Few-shot {}, All {}".format(np.round(many_shot_acc * 100, decimals=2),
-                                                                     np.round(medium_shot_acc * 100, decimals=2),
-                                                                     np.round(few_shot_acc * 100, decimals=2),
-                                                                     np.round(eval_acc_mic_top1 * 100, decimals=2)))
-    return np.round(many_shot_acc * 100, decimals=2), np.round(medium_shot_acc * 100, decimals=2), np.round(
-        few_shot_acc * 100, decimals=2), np.round(eval_acc_mic_top1 * 100, decimals=2)
 
 
 if __name__ == '__main__':
